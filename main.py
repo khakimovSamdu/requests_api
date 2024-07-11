a25a679592a3e568d86e24d2b088ac6155035649
khakimovSamdu <allamurodxakimov@gmail.com>
2024-07-11 10:46:04 +0500

    requests data

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import requests
 
+url = 'https://randomuser.me/api/'
+data = requests.get(url=url)
+print(data.status_code)
+print(type(data))
+
+if data.status_code==200:
+    data = data.json()
+    print(data)
+else:
+    print('Status error')
+    
