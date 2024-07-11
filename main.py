18b14fa4a3d4ecf9ad6ec62585211196ac73bd11
khakimovSamdu <allamurodxakimov@gmail.com>
2024-07-11 11:33:07 +0500

    user datas

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
 
 url = 'https://randomuser.me/api/'
-data = requests.get(url=url)
-print(data.status_code)
-print(type(data))
+respons = requests.get(url=url)
+# print(data.status_code)
+# print(type(data))
 
-if data.status_code==200:
-    data = data.json()
-    print(data)
+if respons.status_code==200:
+    data = respons.json()['results'][0]
+    text = respons.text
+    user = {
+        'fullname': data['name']['first'] + ' ' + data['name']['last'],
+        'email': data['email'],
+        'phone': data['phone'],
+        'image': data['picture']['medium'],
+        'adress': data['location']['city'],
+        'username': data['login']['username']
+    }
+    print(user)
 else:
     print('Status error')
     
